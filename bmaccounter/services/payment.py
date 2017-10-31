28e979879ddf45b4c3d9f158f0dbd3f6133f2e85
hchockarprasad <hchockarprasad@gmail.com>
2017-10-31 19:43:31 +0530

    add account-position validator for payment service

--- bmaccounter/services/payment.py
+++ bmaccounter/services/payment.py
@@ -1,5 +1,12 @@
+from bmcore.models import Account
 
+
+# Payment Service
 class PaymentService(object):
+
+    _permitted_groups_for_debit = [24]
+
+    _permitted_groups_for_credit = [25, 33]
 
     def __init__(self, user):
 
@@ -9,3 +16,22 @@ class PaymentService(object):
     def cash_account(self):
 
         return self.user.userprofile.cash_account
+
+    @property
+    def credit_accounts(self):
+
+        return Account.objects.filter(group__in=self._permitted_groups_for_credit)
+
+    @property
+    def debit_accounts(self):
+
+        return Account.objects.filter(group_id=self._permitted_groups_for_debit)
+
+    def is_valid_position(self, account_id, credit, debit):
+
+        if (credit > 0 and account_id in self.credit_accounts or
+                debit > 0 and account_id in self.debit_accounts):
+
+            return True
+
+        return False
